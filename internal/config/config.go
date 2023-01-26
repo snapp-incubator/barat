997f83281dd0ac76b98b8123a7e81f35922879f1
Nima Mashhadi <nima.mashhadi@snapp.cab>
2023-01-26 23:56:16 +0330

    add config package

---
+++ internal/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+var C *Config
+
+type Config struct {
+	ProjectPath  string        `yaml:"project-path"`
+	TomlPaths    []string      `yaml:"toml-path"`
+	Exclude      Exclude       `yaml:"exclude"`
+	MessageFuncs []MessageFunc `yaml:"message-func"`
+	Options      Opts          `yaml:"options"`
+}
+
+type Exclude struct {
+	Folders  []string `yaml:"folders"`
+	RegexKey []string `yaml:"regex-key"`
+}
+
+type MessageFunc struct {
+	Name        string `yaml:"name"`
+	MessageIDNo int    `yaml:"message-id-no"`
+}
+
+func ToMessageFuncs(args map[string]int) []MessageFunc {
+	var tmp []MessageFunc
+	for k, v := range args {
+		tmp = append(tmp, MessageFunc{Name: k, MessageIDNo: v})
+	}
+	return tmp
+}
+
+type Enable struct {
+	TomlCheck        bool `yaml:"toml-check"`
+	DescriptionCheck bool `yaml:"description-check"`
+	OtherKeyCheck    bool `yaml:"other-key-check"`
+	CodeCheck        bool `yaml:"code-check"`
+}
+
+type Opts struct {
+	Enable Enable `yaml:"enable"`
+}
+
+func LoadConfig(filepath string) error {
+	data, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(data, &C)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
